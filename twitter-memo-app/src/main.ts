5407508ccc8d4e415d99ea8ad18e1a3c5adcce4c
wakky-alcedo <wakky.alcedo@gmail.com>
2025-06-27 21:14:42 +0900

    move: gemini 以外の試作を削除し，gemini を一番上のディレクトリに移動

--- twitter-memo-app/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './registerServiceWorker'  // ← PWA登録を追加
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
--- twitter-memo-app/src/registerServiceWorker.ts
+++
@@ -1,8 +0,0 @@
-// src/registerServiceWorker.ts
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(() => {
-    console.log('Service Worker Registered');
-  }).catch((error) => {
-    console.log('Service Worker registration failed: ', error);
-  });
-}
